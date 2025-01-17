55b0c30582c6d3c39c78850ca1af57f57c02c618
Kalyan <kalyankathuroji@gmail.com>
2025-01-17 19:03:05 +0530

    version 1.1v

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# Welcome to DSA project which includes Major Data Structures like Graphs, Maps etc..
+# Welcome to CashFlowMinimizer DSA project which includes Major Data Structures like Graphs, Maps etc.. It's Objective is to reduce the number of Transactions in between banks with same type of transaction it has.
 
 1. Initial phase is to create a Bank Class with attributes as "bank name", "Net amt" and "types of payment modes".
+2. Coming to Second Phase We have started in writing the main() function Because it is the starting point for any Java application.
+3. In main Function At first I have created an Object for Scanner class for manual Input.
+4. Next I have Taken an input for the Number of banks that are participating in the transactions.
+5. Next I have created an Array of Banks class with an argument as Number of Banks.
+6. Next I have created a Map Object which is helpful for storing the "Bank Name and The number of payment modes it Contains".
--- cashFlowMinimizer.java
+++ cashFlowMinimizer.java
@@ -4,8 +4,90 @@ class Bank{
     public int netAmount;
     public Set<String> types;
 }
+class Pair<K,V>{
+    private K key;
+    private V value;
+    public Pair(K key,V value){
+        this.key=key;
+        this.value=value;
+    }
+    public K getKey(){
+        return key;
+    }
+    public void setKey(K key){
+        this.key=key;
+    }
+    public V getValue(){
+        return value;
+    }
+    public void setValue(V value){
+        this.value=value;
+    }
+}
 public class cashFlowMinimizer{
     public static void main(String args[]){
+      System.out.println("\t\t************ Welcome to Cash Flow Minimizer System ************\n\n");
+      System.out.println("This system minimizes the number of transactions among multiple banks in the different corners of the world that use different modes of payment. There is one world bank (with all payment modes) to act as an intermediary between banks that have no common mode of payment.\n")
+      System.out.println("Enter the number of banks for the transactions");
+      
+      Scanner sc = new Scanner(System.in);
+      int numBanks = sc.nextInt();
+      Bank[] banks =new Bank[numBanks];
 
+      Map<String,Integer> map=new HashMap<>();
+      System.out.println("Enter the details of the banks and transactions as stated:");
+      System.out.println("Bank name, number of payment modes it has, and the payment modes.");
+      System.out.println("Bank name and payment modes should not contain spaces.");
+        
+      int maxNumtypes =0;
+
+      for(int i=0;i<numBanks;i++){
+        if(i==0){
+            System.out.println("World Bank");
+        }
+        else{
+            System.out.println(" Bank "+i+": ");
+        }
+        String name=sc.next();
+        banks[i]=new Bank();
+        banks[i].name=name;
+        map.put(name,i);
+
+        int numtypes=sc.nextInt();
+        
+        // It is One of the Edge Test Case
+        if (i == 0){ maxNumtypes = numtypes;}
+
+        Set<String> types =new HashSet<>(numtypes);
+
+        for(int j=0;j<numtypes;j++){
+            types.add(sc.next());
+        }
+        banks[i].types=types;
+      }
+      System.out.println("Enter the number of transactions");
+      int numTransactions=sc.nextInt();
+      int[][] graph = new int[numBanks][numBanks];
+
+      System.out.println("Enter the details of the transactions as stated:");
+      System.out.println("Bank name from which the transaction originates, bank name to which the transaction is made, and the amount.");
+      
+      for(int i=0; i<numTransactions;i++){
+        String from = sc.next();
+        String to = sc.next();
+        int amt=sc.nextInt();
+        Integer fromIndex=map.get(from);
+        Integer toIndex=map.get(to);
+
+        if(fromIndex!=null && toIndex!=null){
+            int fromIdx = fromIndex.intValue();
+            int toIdx = toIndex.intValue();
+            graph[fromIdx][toIdx]=amt;
+        }else{
+               System.out.println("Invalid Bank Name "+from+" , "+to);
+        }
+      }
+      minimizeCashFlow(numBanks,banks,map,numTransactions,graph,maxNumtypes);
+      sc.close();
     }
 }
